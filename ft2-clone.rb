41568f6892a59564f271b07b860c3e0ddb96785b
Alex Myczko <alex@aiei.ch>
2022-01-13 15:50:42 +0100

    Create ft2-clone.rb

---
+++ ft2-clone.rb
@@ -0,0 +1,11 @@
+cask "ft2-clone" do
+  version "1.50"
+  sha256 "9ddb1293564a6f0f263e46b79b9619c427d2d9bdd15d9af09e38d296c995a55c"
+
+  url "https://16-bits.org/ft2-clone-v#{version}-macos.zip"
+  name "ft2-clone"
+  desc "Music tracker clone of Fasttracker II for modern computers"
+  homepage "https://16-bits.org/ft2.php"
+
+  app "ft2-clone-macos"
+end
