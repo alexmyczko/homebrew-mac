0878d04e1629eec99787f8abbc60b36a859f0506
Alex Myczko <gurkan@phys.ethz.ch>
2024-02-13 19:07:54 +0100

    Update ft2-clone.rb

--- Casks/ft2-clone.rb
+++ Casks/ft2-clone.rb
@@ -1,6 +1,6 @@
 cask "ft2-clone" do
-  version "1.50"
-  sha256 "9ddb1293564a6f0f263e46b79b9619c427d2d9bdd15d9af09e38d296c995a55c"
+  version "1.75"
+  sha256 "577cb39ff34ec8e86651e099f93a7dfd60e3a24d805fc5585d0415dbc5c1429a"
 
   url "https://16-bits.org/ft2-clone-v#{version}-macos.zip"
   name "ft2-clone"
