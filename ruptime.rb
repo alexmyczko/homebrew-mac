99415c54465a139c79a06f516550f2308c09de1f
Alex Myczko <gurkan@phys.ethz.ch>
2024-02-19 16:26:43 +0100

    Update ruptime.rb

--- ruptime.rb
+++ ruptime.rb
@@ -1,18 +1,16 @@
 class Ruptime < Formula
   desc "poor man’s ruptime"
   homepage "https://wedonthaveaprivacyproblem.com"
-  url "https://github.com/alexmyczko/ruptime/archive/refs/tags/1.3.1.tar.gz"
+  url "https://github.com/alexmyczko/ruptime/archive/refs/tags/1.6.tar.gz"
   sha256 "c7eb25831edf1893f3b513c5c38ef5ff5a06078a05b09961c0629d5e77648ce0"
   license "AGPL-3.0"
 
   depends_on "xz"
-  depends_on "mcrypt"
   depends_on "memtester"
-  depends_on "datamash"
 
   def install
     bin.install "ruptime"
-    bin.install "ruptimed"
+    #bin.install "ruptimed"
     bin.install_symlink "ruptime" => "rsw"
     bin.install_symlink "ruptime" => "rhw"
     bin.install_symlink "ruptime" => "runame"
@@ -24,7 +22,7 @@ class Ruptime < Formula
     bin.install_symlink "ruptime" => "rac"
     bin.install_symlink "ruptime" => "rwho"
     man1.install "man/ruptime.1"
-    man8.install "man/ruptimed.8"
+    #man8.install "man/ruptimed.8"
   end
 
   test do
