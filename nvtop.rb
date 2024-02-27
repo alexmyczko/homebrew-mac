c616bf3ace5ed68c31e14bf657bba1212cc7c546
Alex Myczko <gurkan@phys.ethz.ch>
2024-02-27 10:35:57 +0100

    Update nvtop.rb

--- nvtop.rb
+++ nvtop.rb
@@ -1,8 +1,8 @@
 class Nvtop < Formula
   desc "Interactive GPU process monitor"
   homepage "https://github.com/Syllo/nvtop"
-  url "https://sid.ethz.ch/debian/nvtop/macos/nvtop_3.0.2%2Bgit20240213%2Bds.orig.tar.xz"
-  sha256 "fb46aa755eeeadab6e6f511915ab0166f813fb0c1d2dd929bf3a3249b6ae10d0"
+  url "https://github.com/Syllo/nvtop/archive/refs/tags/3.1.0.tar.gz"
+  sha256 "9481c45c136163574f1f16d87789859430bc90a1dc62f181b269b5edd92f01f3"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
