597ca3c1fc48f38770c351ff82d614e67905959c
Alex Myczko <gurkan@phys.ethz.ch>
2024-05-08 14:23:17 +0200

    Update dwarfs.rb

--- dwarfs.rb
+++ dwarfs.rb
@@ -18,6 +18,16 @@ class Dwarfs < Formula
   depends_on "ronn" => :build
   depends_on "xz" => :build
   depends_on "double-conversion" => :build
+  depends_on "ninja" => :build
+  depends_on "python3" => :build
+  depends_on "howard-hinnant-date" => :build
+  depends_on "glog" => :build
+  depends_on "libevent" => :build
+  depends_on "flac" => :build
+  depends_on "openssl" => :build
+  depends_on "utf8cpp" => :build
+  depends_on "boost" => :build
+  depends_on "zstd" => :build
 
   def install
     system "cmake", "."
