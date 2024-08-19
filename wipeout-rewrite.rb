782bec9137493c6f7d404ae1a6caca2c56a564f4
Alex Myczko <gurkan@phys.ethz.ch>
2024-08-19 17:46:11 +0200

    Create wipeout-rewrite.rb

---
+++ wipeout-rewrite.rb
@@ -0,0 +1,21 @@
+class WipeoutRewrite < Formula
+  desc "futuristic anti gravity racing"
+  homepage "https://github.com/phoboslab/wipeout-rewrite"
+  url "http://bananas.debian.net/debian/wipeout/wipeout-rewrite_0.tar.xz"
+  sha256 "535ed89bad1e525407ffa73fbd252644d5b9a848eddd0b0af98a4c838c69998c"
+  license "PUBLIC-DOMAIN"
+  # get the data from https://phoboslab.org/files/wipeout-data-v01.zip
+
+  depends_on "cmake" => :build
+  depends_on "sdl2" => :build
+
+  def install
+    system "make"
+    bin.install "wipegame"
+    #man1.install "manpage/wipegame" => "wipegame.6"
+  end
+
+  test do
+    system "#{bin}/wipegame", "-v"
+  end
+end
