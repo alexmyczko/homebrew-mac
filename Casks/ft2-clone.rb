2bea1387cbe2367ec8b3c40011ee4de1c1e71e41
Giovanni Merlino <gmerlino@unime.it>
2025-05-03 23:07:54 +0200

    Update ft2-clone to version 1.95

--- Casks/ft2-clone.rb
+++ Casks/ft2-clone.rb
@@ -1,6 +1,6 @@
 cask "ft2-clone" do
-  version "1.75"
-  sha256 "577cb39ff34ec8e86651e099f93a7dfd60e3a24d805fc5585d0415dbc5c1429a"
+  version "1.95"
+  sha256 "e0712739e95a1e2a3bc4c7ac6af433da5337bb0371defca0ac88eb5b8e84337a"
 
   url "https://16-bits.org/ft2-clone-v#{version}-macos.zip"
   name "ft2-clone"
